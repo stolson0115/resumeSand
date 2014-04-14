92ae5be0a0b05739a415a2d6eaba9757637c7b10
root <sethtfo@gmail.com>
2014-04-14 15:08:16 -0300

    Static_Pages 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :projects do
     resources :comments
   end
-  root 'welcome#index'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
