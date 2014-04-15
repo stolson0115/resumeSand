34a202088acf1041afa4be40722b9aee3444129f
root <sethtfo@gmail.com>
2014-04-15 09:04:54 -0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     resources :comments
   end
   root 'static_pages#home'
-
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
