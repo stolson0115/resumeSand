5c04057a858dbfe6b9df4d1ac599971934edebbb
root <sethtfo@gmail.com>
2014-04-15 14:05:22 -0300

    1

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
-	validates :title, presence: true, length: {minimum:5}
-	validates :category, presence: true
-	validates :text, presence:true	
+	validates :title, presence: true, length: {minimum:5},length: {maximum:140}
+	validates :category, presence: true,length: {maximum:140}
+	validates :text, presence:true, length: {maximum:140}	
 end
