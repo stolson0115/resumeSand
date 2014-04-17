10be242c8871a5ee5f56bff886047fcfde38c9db
Seth Olson <sethtfo@gmail.com>
2014-04-17 09:18:40 -0300

    Attempt fix Gmail Error 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
-
+config.action_mailer.default_url_options = { :host => 'thawing-plateau-4759.herokuapp.com' }
+Rails.application.routes.default_url_options[:host] = 'thawing-plateau-4759.herokuapp.com'
 config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
