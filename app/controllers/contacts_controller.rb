da8ce8cfe2e4b6841390829b29e386852decc6c6
Seth Olson <sethtfo@gmail.com>
2014-04-17 15:20:28 -0300

    Form Layout Edit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,8 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      redirect_to root_path
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      redirect_to root_path
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
