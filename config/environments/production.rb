1a9204712f94db20eefa83f7d20fc9eefaf734a1
Seth Olson <sethtfo@gmail.com>
2014-04-17 12:46:11 -0300

    text

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-config.action_mailer.default_url_options = { :host => 'thawing-plateau-4759.herokuapp.com' }
-Rails.application.routes.default_url_options[:host] = 'thawing-plateau-4759.herokuapp.com'
+config.action_mailer.default_url_options = { :host => 'http://thawing-plateau-4759.herokuapp.com/' }
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
