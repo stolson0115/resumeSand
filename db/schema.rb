ce86d93f1587ce785a68cadf8c72c914aff97ecd
Seth Olson <sethtfo@gmail.com>
2014-04-22 13:08:24 -0300

    hmm

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140422154035) do
 
-  create_table "comments", force: true do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "project_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["project_id"], name: "index_comments_on_project_id"
-
   create_table "projects", force: true do |t|
     t.string   "title"
     t.string   "category"
