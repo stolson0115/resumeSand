4930ba98b567a35718da78a33e6d328c107fc7c7
Seth Olson <sethtfo@gmail.com>
2014-04-24 14:15:28 -0400

    fixed image controller

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,2 @@
 class Image < ActiveRecord::Base
-	after_save :set_orientation
-	has_attached_file :data, :styles => { :large => "800x", :thumb => "100x100#"}
-	validates_attachment_content_type :data
-	private
-	def set_orientation
-		self.orientation=Paperlcip::Geometry.from_file(self.data.to_file).horizontal? ? 'horizontal' : 'vertical'
-	end
 end
