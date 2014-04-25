ccd0e692bac28a8e5bfb460048139bb712bf2b2c
Seth Olson <sethtfo@gmail.com>
2014-04-25 11:30:22 -0400

    changed sliderImage

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,8 @@
 class ImagesController < ApplicationController
 	def index
 		@images=Dir.glob("app/assets/images/p_*")
-		@thumbs=Dir.glob("app/assets/images/thumb_*")		
+		@thumbs=Dir.glob("app/assets/images/thumb_*")
+		$ind=@images.size
 	end
 	def show
 		path = "app/assets/images/#{params[:id]}.jpg"
