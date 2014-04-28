0fe8f091952cb9825f0192f3cefcf566bf26eca6
Seth Olson <sethtfo@gmail.com>
2014-04-28 12:06:53 -0400

    projects style brush

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum:5},length: {maximum:140}
 	validates :category, presence: true,length: {maximum:140}
-	validates :text, presence:true, length: {maximum:140}	
+	validates :text, presence:true	
 end
