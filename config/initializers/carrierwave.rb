834232859252643ba57a3cc59a06ff035f11011d
Seth Olson <sethtfo@gmail.com>
2014-04-30 07:45:06 -0400

    fixed carrierwaveConfig

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-<<<<<<< HEAD
     :aws_access_key_id      => 'AWS_ACCESS_KEY_ID',       # required
     :aws_secret_access_key  => 'AWS_SECRET_ACCESS_KEY',       # required
-=======
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],       # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
->>>>>>> origin/master
     :region                 => 'us-west-2',  # optional, defaults to 'us-east-1'
    # :host                   => 'https://s3.amazonaws.com', 
     #:endpoint               => 'sethmile.s3-website-us-west-2.amazonaws.com'
